59ae4571e054ae590047cdca315efdeb6d79a64e
michalW93 <michal.walkowiak93@gmail.com>
2021-12-14 12:10:06 +0100

    refactoring

--- src/main/java/Generator.java
+++ src/main/java/Generator.java
@@ -1,13 +1,11 @@
 public class Generator implements UsGenerator {
     UsIsin usIsin = new UsIsin();
     UsCusip usCusip = new UsCusip();
-    private String isin;
 
     @Override
     public String generateIsin(String lastIsin) {
         String cusipFromIsin = usCusip.getCusipFromIsin(lastIsin);
-        isin = usIsin.generate(usCusip.generate(cusipFromIsin));
-        return isin;
+        String cusip = usCusip.generate(cusipFromIsin);
+        return usIsin.generate(cusip);
     }
-
 }
